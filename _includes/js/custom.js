821681b5f16bf46aebd6099237bf23e192708146
brgh <71097588+brgh@users.noreply.github.com>
2024-12-08 14:48:16 -0800

    Updates to custom.js file. This should solve the broken css file load

--- _includes/js/custom.js
+++ _includes/js/custom.js
@@ -64,29 +64,45 @@ function getAdjTimestampCell(checkbox) {
 // }
 
   // Check if dark mode is already set in localStorage when the page loads
-  document.addEventListener('DOMContentLoaded', function() {
-    const storedTheme = localStorage.getItem('colorTheme');
-    const currentTheme = jtd.getTheme();
-    console.log(`Current theme: ${currentTheme}`);
-    console.log(`Stored theme: ${storedTheme}`);
-    if (storedTheme === currentTheme) {
-        return;
+  document.addEventListener('DOMContentLoaded', () => { // add event listener for DOMContentLoaded event
+    
+    // grab the colorTheme value from localStorage and store in storedTheme 
+    // if null then return and do nothing (no theme selected so keep default)
+    const storedTheme = localStorage.getItem('colorTheme'); 
+    if (!storedTheme) {
+      return;
     }
-    console.log(storedTheme === currentTheme)
-    // if (storedTheme === 'dark') {
-      jtd.setTheme(storedTheme);
-    // } else {
-    //     jtd.setTheme('light');
-    // }
+    console.log(`Stored theme: ${storedTheme}`); // for troubleshooting 
+
+    // get the current theme and store in currentTheme 
+    // if it is same as storedTheme then return and do nothing (keep default) 
+    const currentTheme = jtd.getTheme(); 
+    console.log(`Current theme: ${currentTheme}`); // for troubleshooting 
+    console.log(storedTheme === currentTheme);     // for troubleshooting 
+    if (storedTheme === currentTheme) {
+      return;
+    }
+    
+    // finally set the theme to storedTheme 
+    console.log(`Setting the theme to ${storedTheme}`);
+    jtd.setTheme(storedTheme);
   });
 
-  // Toggle dark mode on button click and save preference
+  // This is the function we will run when the user clicks the button toggle for dark mode 
   function changeTheme() {
+    // get the current theme 
     const currentTheme = jtd.getTheme(); 
     console.log(`Current theme: ${currentTheme}`); 
-    newTheme = currentTheme === "dark" ? "light" : "dark";
+
+    // set newTheme based on value of currentTheme (if currentTheme dark then set to default 
+    // and if default, set to dark)
+    const newTheme = currentTheme === "dark" ? "default" : "dark";
     console.log(`New theme: ${newTheme}`); 
+
+    // save the theme to localStorage with the colorTheme key 
     localStorage.setItem('colorTheme', newTheme);
     console.log(localStorage.getItem('colorTheme')); 
+
+    // finally, set the theme 
     jtd.setTheme(newTheme); 
 }
